c51a1bdb30036f8c1fa3c0a249de9a7607ebe4df
Morris Vanegas <morris.vanegas@gmail.com>
2020-05-27 12:34:39 -0400

    Update tessellateModule.m to check if srcposns was defined

--- lib/tessellateModule.m
+++ lib/tessellateModule.m
@@ -2,15 +2,20 @@ function [probe] = tessellateModule( probe )
 %TESSELATEMODULE Summary of this function goes here
 %   Detailed explanation goes here
 
-
+optexist = true;    % flag to determine if optodes were defined. 
+if( isfield(probe.module, 'srcposns') == false )
+    optexist = false;
+end
 
 if (strcmp(probe.module.shape, 'square'))   
     single_row_quantities = probe.n_modules_x;
     n_modules = probe.n_modules_x * probe.n_modules_y;
     
     modules = zeros(n_modules, 4); % x_centroid,y_centroid, orientation, active
-    allsrcs = zeros(size(probe.module.srcposns(:,1), 1) * n_modules, 4); % x,y,moduleIdx,globalsrcIdx
-    alldets = zeros(size(probe.module.detposns(:,1), 1) * n_modules, 4); % x,y,moduleIdx,globaldetIdx
+    if(optexist)
+        allsrcs = zeros(size(probe.module.srcposns(:,1), 1) * n_modules, 4); % x,y,moduleIdx,globalsrcIdx
+        alldets = zeros(size(probe.module.detposns(:,1), 1) * n_modules, 4); % x,y,moduleIdx,globaldetIdx
+    end
     
     modcount = 1;
     srccount = 1;
@@ -29,26 +34,28 @@ if (strcmp(probe.module.shape, 'square'))
             modules(modcount,:) = [x,y,orientation,active]; % x, y, orientation (deg), active
             
             
-            % translate and rotate sources. Save which module idx they
+            % translate and rotate sources, if they exist. Save which module idx they
             % belong to. give a unique id to each source
-            nsrcs = size(probe.module.srcposns(:,1), 1);
-            % rotatedsrcs = rotateCoordinates(probe.module.srcposns,
-            % angle);
-            translatedsrcs = translateCoordinates(probe.module.srcposns, [x,y]);
-            moduleidxsrcs = modcount; 
-            for s=1:nsrcs
-                allsrcs(srccount,:) = [translatedsrcs(s,:), moduleidxsrcs, srccount];
-                srccount = srccount+1;
-            end
-            
-            % translate and rotate detectors. Save which module idx they
-            % belong to. give a unique id to each detector
-            ndets = size(probe.module.detposns(:,1), 1);
-            translateddets = translateCoordinates(probe.module.detposns, [x,y]);
-            moduleidxdets = modcount;
-            for d=1:ndets
-                alldets(detcount,:) = [translateddets(d,:), moduleidxdets, detcount];
-                detcount = detcount+1;
+            if (optexist)
+                nsrcs = size(probe.module.srcposns(:,1), 1);
+                % rotatedsrcs = rotateCoordinates(probe.module.srcposns,
+                % angle);
+                translatedsrcs = translateCoordinates(probe.module.srcposns, [x,y]);
+                moduleidxsrcs = modcount; 
+                for s=1:nsrcs
+                    allsrcs(srccount,:) = [translatedsrcs(s,:), moduleidxsrcs, srccount];
+                    srccount = srccount+1;
+                end
+
+                % translate and rotate detectors. Save which module idx they
+                % belong to. give a unique id to each detector
+                ndets = size(probe.module.detposns(:,1), 1);
+                translateddets = translateCoordinates(probe.module.detposns, [x,y]);
+                moduleidxdets = modcount;
+                for d=1:ndets
+                    alldets(detcount,:) = [translateddets(d,:), moduleidxdets, detcount];
+                    detcount = detcount+1;
+                end
             end
             
             % increment module
@@ -60,12 +67,11 @@ end
 
 
 % Update probe values
-%probe.n_modules = n_modules;
 probe.modules = modules;
-%probe.single_row_quantities = single_row_quantities;
-
-probe.srcposns = allsrcs;
-probe.detposns = alldets;
+if (optexist)
+    probe.srcposns = allsrcs;
+    probe.detposns = alldets;
+end
     
     
 end
