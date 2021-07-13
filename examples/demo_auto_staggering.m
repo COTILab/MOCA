4fe33c79bdc46b4990af4544bb4cb1cdf02bbf4e
Morris Vanegas <morris.vanegas@gmail.com>
2021-07-13 14:40:35 -0400

    Update demo_auto_staggering to use exhaustStaggering

--- examples/demo_auto_staggering.m
+++ examples/demo_auto_staggering.m
@@ -9,7 +9,7 @@ y1 = 10; pos = 14;
 probe.module.srcposns = [pos, pos; -pos, -pos];
 probe.module.detposns = [-pos, pos; pos, -pos]; 
 
-probe.roi = createROI(40,120); % width and height
+probe.roi = createROI(80,120); % width and height
 probe.spacing = 0;
 probe.sdrange = [10 40];
 probe = createLayout(probe); 
@@ -19,68 +19,52 @@ probe = characterizeProbe(probe);
 figure
 plotProbe(probe);
 
-%%
-fig = figure
-c = 1;
-t_offsets = 0:5:40;
+%% Analyze staggering
+modulesToStagger = [3,4];
+staggerAmount = [0:42];
 
-for t=t_offsets
-    % reset layout
-    clf(fig)
-    probe = createLayout(probe); 
-    
-    % vary the staggering
-    probe = translateModules(probe, [2], [t 0]);
-    
-    % Re-characterize and save the results structure
-    probe = characterizeProbe(probe);
-    output(c).results = probe.results;
-    
-    % save individual metrics
-    channels(c) = size(output(c).results.channels,1);
-    intrachannels(c) = size(output(c).results.intrachannels,1);
-    interchannels(c) = size(output(c).results.interchannels,1);
-    brainsensitivity(c) = mean( output(c).results.brainsensitivity(:,1) );
-    intrabrainsensitivity(c) = mean( output(c).results.intrabrainsensitivity(:,1) );
-    interbrainsensitivity(c) = mean( output(c).results.interbrainsensitivity(:,1) );
-    ngroups(c) = output(c).results.ngroups;
-    
-    % visual display
-    plotProbe(probe); 
-    title(strcat('Staggering: ',num2str(t),'mm'))
-    pause(.01)
-    c= c+1;
-    
+[cfgs] = exhaustStaggering(probe, modulesToStagger, staggerAmount)
+
+
+%% save individual metrics and plot
+for i = 1:size(cfgs,2)
+    channels(i) = size(cfgs(i).results.channels,1);
+    intrachannels(i) = size(cfgs(i).results.intrachannels,1);
+    interchannels(i) = size(cfgs(i).results.interchannels,1);
+    brainsensitivity(i) = mean( cfgs(i).results.brainsensitivity(:,1) );
+    intrabrainsensitivity(i) = mean( cfgs(i).results.intrabrainsensitivity(:,1) );
+    interbrainsensitivity(i) = mean( cfgs(i).results.interbrainsensitivity(:,1) );
+    ngroups(i) = cfgs(i).results.ngroups;
 end
-
-%% Create visual plots of exhaustive search
 
 % Channels
 figure
-plot(t_offsets, channels, '*-')
+plot(staggerAmount, channels, '*-')
 xlabel('Offset staggering [mm]');
 ylabel('Number of channels');
 title('Number of channels per staggering amount');
 
 % Inter-module Channels
 figure
-plot(t_offsets, interchannels, '*-')
+plot(staggerAmount, interchannels, '*-')
 xlabel('Offset staggering [mm]');
 ylabel('Number of channels');
 title('Number of inter-module channels per staggering amount');
 
 % Brain Sensitivity
 figure
-plot(t_offsets, brainsensitivity, '*-')
+plot(staggerAmount, brainsensitivity, '*-')
 xlabel('Offset staggering [mm]');
 ylabel('Average Brain Sensitivity');
 title('Average Brain Sensitivity per staggering amount');
 maxBSval = max(brainsensitivity);
-maxBSidx = find(brainsensitivity == maxBSval);
+maxBSidx = find(brainsensitivity == maxBSval)-1;
+hold on
+plot(maxBSidx, maxBSval, 'r*');
 
 % Number of SMGs
 figure
-plot(t_offsets, ngroups, '*-')
+plot(staggerAmount, ngroups, '*-')
 xlabel('Offset staggering [mm]');
 ylabel('Number of Spatial Multiplexing Groups');
 title('Number of SMGs per combination');
