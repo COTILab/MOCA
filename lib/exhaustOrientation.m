396def6f45089f4e4516236f4a8e2fe5b0bfa24d
Morris Vanegas <morris.vanegas@gmail.com>
2021-07-13 14:23:30 -0400

    Create exhaustOrientation.m

---
+++ lib/exhaustOrientation.m
@@ -0,0 +1,78 @@
+function [cfgs] = exhaustOrientation(probe)
+%EXHAUSTORIENTATION Exhaustively analyze all possible module orientations
+%   Takes in a probe and exhaustively changes each module orientation. Does
+%   this for all possible combinations of modules. The number of possible
+%   configurations is A^B, where A is the number of orientations (or sides)
+%   or a module, and B is the number of modules. For example, a 2x3 probe
+%   of squares has 4^6=4096 unique possible ways in which to orient each of
+%   the modules. This script displays the probe and the re-oriented modules
+%   to the user. It saves the number of channels, average brain
+%   sensitivity, and the number of spatial multiplexing groups for each
+%   configuration. Yeah, it's exhaustive. 
+
+
+% setup based on module shape and quantity
+switch probe.module.shape
+    case 'square'
+        nOrientations = 4;
+    case 'triangle'
+        nOrientations = 3;
+    case 'hexagon'
+        nOrientations = 6;
+    otherwise
+        nOrientations = size(probe.module.perimeter,1);
+end
+degrees = 360/nOrientations;
+orientations = [degrees:degrees:360] - degrees;
+nModules = probe.results.modulecount;
+nConfigurations = nOrientations^nModules;
+
+
+% create an array of orientations
+for v=1:nModules
+    vectors{v} = orientations;
+end
+nVectors = numel(vectors);          % number of vectors
+combinations = cell(1,nVectors);           % pre-define to generate comma-separated list
+[combinations{end:-1:1}] = ndgrid(vectors{end:-1:1}); % the reverse order in these two
+% comma-separated lists is needed to produce the rows of the result matrix 
+combinations = cat(nVectors+1, combinations{:});  % concat the n n-dim arrays along dimension n+1
+combinations = reshape(combinations,[],nVectors); % reshape to obtain desired matrix
+
+
+% Run through all combinations
+fig = figure;    
+    
+for c=1:size(combinations,1) %permutations
+    % clear and reset probe
+    clf(fig)
+    probe = createLayout(probe); % reset back to basic
+    
+    % change orientation of modules based on combination
+    for m=1:nModules
+        probe = rotateModules(probe, [m], combinations(c,m));
+    end
+    
+    % Re-characterize and save the results structur
+    probe = characterizeProbe(probe);
+    cfgs(c).results = probe.results;
+    
+    % save individual metrics
+%     channels(c) = size(cfgs(c).results.channels,1);
+%     intrachannels(c) = size(cfgs(c).results.intrachannels,1);
+%     interchannels(c) = size(cfgs(c).results.interchannels,1);
+%     brainsensitivity(c) = mean( cfgs(c).results.brainsensitivity(:,1) );
+%     intrabrainsensitivity(c) = mean( cfgs(c).results.intrabrainsensitivity(:,1) );
+%     interbrainsensitivity(c) = mean( cfgs(c).results.interbrainsensitivity(:,1) );
+%     ngroups(c) = cfgs(c).results.ngroups;
+    
+    % visual display
+    plotProbe(probe); plotROI(probe)
+    title(strcat('Config: ',num2str(c),'/',num2str(nConfigurations)))
+    pause(.01)
+end
+
+
+
+end
+
