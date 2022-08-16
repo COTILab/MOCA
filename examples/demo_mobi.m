1a924d6d90f06cb3f62219feebe86a2ca634b3cd
Morris Vanegas <morris.vanegas@gmail.com>
2022-08-16 14:29:32 -0400

    Update demo_mobi.m

--- examples/demo_mobi.m
+++ examples/demo_mobi.m
@@ -1,34 +1,41 @@
-% Outputs a single row 2 MOBI-module probe
+% Basic MOBI functionality
 
 
 %% Original, "straight line" probe
-% Module geometry - MOBI Module
+% 1 - Module geometry - MOBI Module - Blue icons in flowchart inside MOCA
+% github
 probe.module = createModule("MOBI", 53);
 probe.module.srcposns = [16,0; -16,0; 0,11;];
 probe.module.detposns = [0,19;0,-19];
 probe.spacing = 4; % adding 2mm of silicone in circuit cover
-probe.roi = createROI(100,50-(-30)); % width and height
+probe.roi = createROI(200,80); % width and height
     
+
+% 2 - Assembly processes
 % Probe
-probe = createLayout(probe); 
+probe = createLayout(probe); % assembly processes
 triheight = 50*sind(60);
     
 %%%%% Probe Layout 6 %%%% - 8 modules total
-probe = rotateModules(probe, [1:2], 120);
-    
-%probe = translateProbe(probe, -1*abs([minx, miny]));
-%probe = translateProbe(probe, [13.25 -22.9497]);
+probe = rotateModules(probe, [1], 120);
+%probe = rotateModules(probe, [2], -120);
 probe = translateModules(probe, [2], [-4 0]);
 
 % For single row, 3 modules, long channels for SNR
 probe.sdrange = 35; %100;
+probe = toggleModules(probe, [3,4], 'off');
 
 
+
+% 3 - Probe Characterization
 probe = characterizeProbe(probe);
 figure
 moca_plotProbe(probe); 
-%plotROI(probe)
+plotROI(probe)
 probe.results
+
+
+
 
 %% New MOCA scripts to only plot modules/channels as fillers for MOCA
 % Visualization
@@ -56,9 +63,10 @@ for i=1:size(probe.results.interchannels,1)
 end
 
 
+
 %% If plots look good, save it
 mocaprobe = probe;
 save('Single_Row_2module', 'mocaprobe');
 
-%% Create SD structure and export
+% Create SD structure and export
 SD = exportOptodes(mocaprobe, 'mocaexport.SD');
